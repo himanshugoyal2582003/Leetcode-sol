5f658f0713cc38a0930c034e0e35a43a79841335
Himanshu Goyal <81083896+himanshugoyal2582003@users.noreply.github.com>
2026-08-19 10:57:37 +0530

    Time: 2 ms (19.29%), Space: 46.7 MB (62.57%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,24 +1,29 @@
 class Solution {
+
     public List<List<Integer>> levelOrder(TreeNode root) {
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        solve(root, 0, map);
+
         List<List<Integer>> ans = new ArrayList<>();
-        if(root==null){return ans;}
 
-        Deque<TreeNode> q = new ArrayDeque<>();
-        q.offer(root);
-
-        while(!q.isEmpty()){
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-
-            for(int i =0 ; i< size ; i++){
-                TreeNode curr = q.poll();
-                level.add(curr.val);
-                if(curr.left!=null){q.offer(curr.left);}
-                if(curr.right!=null){q.offer(curr.right);}
-            }
-
-            ans.add(level);
+        for (int i = 0; i < map.size(); i++) {
+            ans.add(map.get(i));
         }
+
         return ans;
+    }
+
+    void solve(TreeNode root, int level, Map<Integer, List<Integer>> map) {
+
+        if (root == null) return;
+
+        map.putIfAbsent(level, new ArrayList<>());
+
+        map.get(level).add(root.val);
+
+        solve(root.left, level + 1, map);
+        solve(root.right, level + 1, map);
     }
 }
