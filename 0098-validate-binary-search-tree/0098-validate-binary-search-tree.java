8a6a8192b32e1355ddee84c2ff68385c65cd9ce4
Himanshu Goyal <81083896+himanshugoyal2582003@users.noreply.github.com>
2026-08-20 12:05:41 +0530

    Time: 2 ms (12.8%), Space: 45.7 MB (5.16%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        inorder(root, ans);
+        for(int i =0 ; i<ans.size()-1;i++){
+            if(ans.get(i)>=ans.get(i+1)){return false;}
+        }
+        return true;
+    }
+
+     public void inorder(TreeNode root, List<Integer> ans) {
+        if (root == null) return;
+        inorder(root.left, ans);  
+        ans.add(root.val);        
+        inorder(root.right, ans); 
+    }
+}
