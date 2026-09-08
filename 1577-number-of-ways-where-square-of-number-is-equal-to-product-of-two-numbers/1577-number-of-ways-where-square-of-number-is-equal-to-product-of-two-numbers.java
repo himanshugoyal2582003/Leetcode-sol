6d8f4e517b025348c8806a08e1ac5f222698d341
Himanshu Goyal <81083896+himanshugoyal2582003@users.noreply.github.com>
2026-09-08 11:04:10 +0530

    Time: 17 ms (82.19%), Space: 44.8 MB (83.56%) - LeetHub

--- 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
@@ -1,34 +1,31 @@
 class Solution {
     public int numTriplets(int[] nums1, int[] nums2) {
+        return solve(nums1, nums2) + solve(nums2, nums1);
+    }
+
+    int solve(int[] a, int[] b) {
         int ans = 0;
-        if(nums2.length==1 && nums2[0]==4){
-            return 1;
-        }
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int x : nums1) {
+        for (int x : b)
+            map.put(x, map.getOrDefault(x, 0) + 1);
+
+        for (int x : a) {
             long target = (long) x * x;
-            if(nums2.length==1 && nums2[0]==4){
-            break;
-        }
 
-            for (int i = 0; i < nums2.length; i++) {
-                for (int j = i + 1; j < nums2.length; j++) {
-                    if ((long) nums2[i] * nums2[j] == target) {
-                        ans++;
-                    }
-                }
-            }
-        }
+            for (int y : map.keySet()) {
+                if (target % y == 0) {
+                    long z = target / y;
 
-        for (int x : nums2) {
-            long target = (long) x * x;
- if(nums2.length==1 && nums2[0]==4){
-            break;
-        }
-            for (int i = 0; i < nums1.length; i++) {
-                for (int j = i + 1; j < nums1.length; j++) {
-                    if ((long) nums1[i] * nums1[j] == target) {
-                        ans++;
+                    if (z > Integer.MAX_VALUE) continue;
+
+                    if (map.containsKey((int) z)) {
+                        if (y < z)
+                            ans += map.get(y) * map.get((int) z);
+                        else if (y == z) {
+                            int c = map.get(y);
+                            ans += c * (c - 1) / 2;
+                        }
                     }
                 }
             }
